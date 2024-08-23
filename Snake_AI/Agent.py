b1ede1757d152e56ac5209d37c5315da57a67a60
Jaden Isaac <jadenisaac10@gmail.com>
2024-08-23 15:34:26 +0530

    update

--- Snake_AI/Agent.py
+++ Snake_AI/Agent.py
@@ -6,27 +6,28 @@ from Game import SnakeGameAI, Direction, Point
 from Model import Linear_QNet, QTrainer
 from Helper import plot
 
-MAX_MEMORY = 100_100
+MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
 class Agent:
-    
+
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0 # controls randomness
-        self.gamma = 0 # discount rate
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
-    
+
+
     def get_state(self, game):
         head = game.snake[0]
         point_l = Point(head.x - 20, head.y)
         point_r = Point(head.x + 20, head.y)
         point_u = Point(head.x, head.y - 20)
         point_d = Point(head.x, head.y + 20)
-        
+
         dir_l = game.direction == Direction.LEFT
         dir_r = game.direction == Direction.RIGHT
         dir_u = game.direction == Direction.UP
@@ -34,30 +35,30 @@ class Agent:
 
         state = [
             # Danger straight
-            (dir_r and game.is_collision(point_r)) or 
-            (dir_l and game.is_collision(point_l)) or 
-            (dir_u and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
             (dir_d and game.is_collision(point_d)),
 
             # Danger right
-            (dir_u and game.is_collision(point_r)) or 
-            (dir_d and game.is_collision(point_l)) or 
-            (dir_l and game.is_collision(point_u)) or 
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
             (dir_r and game.is_collision(point_d)),
 
             # Danger left
-            (dir_d and game.is_collision(point_r)) or 
-            (dir_u and game.is_collision(point_l)) or 
-            (dir_r and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
             (dir_l and game.is_collision(point_d)),
-            
+
             # Move direction
             dir_l,
             dir_r,
             dir_u,
             dir_d,
-            
-            # Food location 
+
+            # Food location
             game.food.x < game.head.x,  # food left
             game.food.x > game.head.x,  # food right
             game.food.y < game.head.y,  # food up
@@ -65,10 +66,10 @@ class Agent:
             ]
 
         return np.array(state, dtype=int)
-    
-    def rmrmber(self, state, action, reward, next_state, done):
+
+    def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
-    
+
     def train_long_memory(self):
         if len(self.memory) > BATCH_SIZE:
             mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
@@ -77,10 +78,12 @@ class Agent:
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
+        #for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
 
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
-    
+
     def get_action(self, state):
         # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_games
@@ -96,7 +99,7 @@ class Agent:
 
         return final_move
 
-    
+
 def train():
     plot_scores = []
     plot_mean_scores = []
@@ -107,22 +110,22 @@ def train():
     while True:
         # get old state
         state_old = agent.get_state(game)
-        
+
         # get move
         final_move = agent.get_action(state_old)
-        
+
         # perform move and get new state
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
-        
+
         # train short memory
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
-        
+
         # remember
-        agent.rmrmber(state_old, final_move, reward, state_new, done)
-        
+        agent.remember(state_old, final_move, reward, state_new, done)
+
         if done:
-            # train long memory, plot results
+            # train long memory, plot result
             game.reset()
             agent.n_games += 1
             agent.train_long_memory()
@@ -130,14 +133,15 @@ def train():
             if score > record:
                 record = score
                 agent.model.save()
-                
-            print('Game', agent.n_games, 'Score', score, 'Record', record)
-            
+
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
             plot_scores.append(score)
             total_score += score
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
+
 
 if __name__ == '__main__':
     train()
